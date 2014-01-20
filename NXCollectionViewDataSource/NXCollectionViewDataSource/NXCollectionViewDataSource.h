da95a2c29073d0c53789fe60973d50e1223f1147
Tobias Kräntzer <tobias@nxtbgthng.com>
2014-01-20 17:51:15 +0100

    Add some comments

--- NXCollectionViewDataSource/NXCollectionViewDataSource/NXCollectionViewDataSource.h
+++ NXCollectionViewDataSource/NXCollectionViewDataSource/NXCollectionViewDataSource.h
@@ -17,13 +17,37 @@ typedef void(^NXCollectionViewDataSourcePostUpdateBlock)(NXCollectionViewDataSou
 @interface NXCollectionViewDataSource : NSObject <UICollectionViewDataSource>
 
 #pragma mark Life-cycle
+
+/*! Initialize the data source with a collection view.
+ 
+    The data source sets it self as a collection view data source on the
+    collection view and keeps a weak reference to the collection view.
+ 
+    @param collectionView The UICollectionView this data source should manage.
+ */
 - (id)initWithCollectionView:(UICollectionView *)collectionView;
 
 #pragma mark Collection View
+
+/*! A reference to the UICollectionVIew manegd by this data source.
+ */
 @property (nonatomic, readonly, weak) UICollectionView *collectionView;
 
 #pragma mark Register Cell and Supplementary View Classes
+
+/*! Registers a Class used to create the cells for the collection view.
+ 
+    @param cellClass The class of a cell that you want to use in the collection view.
+    @param prepareBlock A Block which is called to prepare the cell. This block is called after the data source internaly dequeues the cell.
+ */
 - (void)registerClass:(Class)cellClass withPrepareBlock:(NXCollectionViewDataSourcePrepareBlock)prepareBlock;
+
+/*! Register a Class used to create supplementary views for the collection view.
+ 
+    @param viewClass The class to use for the supplementary view.
+    @param elementKind The kind of supplementary view to create. This value is defined by the layout object. This parameter must not be nil.
+    @param prepareBlock A Block which is called to prepare the view. This block is called after the data source internaly dequeues the view.
+ */
 - (void)registerClass:(Class)viewClass forSupplementaryViewOfKind:(NSString *)elementKind withPrepareBlock:(NXCollectionViewDataSourcePrepareBlock)prepareBlock;
 
 #pragma mark Getting Item and Section Metrics
