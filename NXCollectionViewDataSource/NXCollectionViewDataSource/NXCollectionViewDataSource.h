6b3c9863b02ee17d1372cd314ead2f5d48bbf66e
Tobias Kräntzer <tobias@nxtbgthng.com>
2014-01-20 14:28:01 +0100

    Setup Project

---
+++ NXCollectionViewDataSource/NXCollectionViewDataSource/NXCollectionViewDataSource.h
@@ -0,0 +1,13 @@
+//
+//  NXCollectionViewDataSource.h
+//  NXCollectionViewDataSource
+//
+//  Created by Tobias Kräntzer on 20.01.14.
+//  Copyright (c) 2014 nxtbgthng GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NXCollectionViewDataSource : NSObject
+
+@end
---
+++ NXCollectionViewDataSource/NXCollectionViewDataSource/NXCollectionViewDataSource.m
@@ -0,0 +1,13 @@
+//
+//  NXCollectionViewDataSource.m
+//  NXCollectionViewDataSource
+//
+//  Created by Tobias Kräntzer on 20.01.14.
+//  Copyright (c) 2014 nxtbgthng GmbH. All rights reserved.
+//
+
+#import "NXCollectionViewDataSource.h"
+
+@implementation NXCollectionViewDataSource
+
+@end
